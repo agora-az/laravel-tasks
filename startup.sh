fc9bbd4ab2a8dc0a595204d0b82351f93df8f54e
staceyrattlesnake <staceyrattlesnake@onenationgov.com>
2026-02-28 23:07:36 -0800

    Added directories for storage

--- startup.sh
+++ startup.sh
@@ -9,6 +9,14 @@ service nginx reload
 
 # Laravel optimizations
 cd /home/site/wwwroot
+
+# Ensure Laravel storage directories exist
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache
+mkdir -p storage/logs
+
+# Fix permissions
 chmod -R 755 storage bootstrap/cache
 php artisan config:cache || true
 php artisan route:cache || true
