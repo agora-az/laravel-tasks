53e92b9dd0e489869de7035fdd76e48b16d3d9cd
staceyrattlesnake <staceyrattlesnake@onenationgov.com>
2026-03-01 00:32:24 -0800

    Added TrustProxies to the app middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
         $middleware->alias([
             'auth.check' => \App\Http\Middleware\CheckAuthenticated::class,
         ]);
